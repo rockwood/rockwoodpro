21f71e1afa67529bc1f2ca4bb8a7e90f9ed98e61
Kevin Rockwood <kevin@rockwood.me>
2013-08-17 16:19:39 -0400

    Add invalid sign up scenario

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 feature 'Sign up' do
-  scenario 'via the sign up page' do
-    user = FactoryGirl.build(:user)
-    sign_up_page = SignUpPage.new
+  let(:sign_up_page){ SignUpPage.new }
+
+  scenario 'with a valid user' do
+    valid_user = FactoryGirl.build(:user)
     sign_up_page.visit_page
-    sign_up_page.fill_form(user)
+    sign_up_page.fill_form(valid_user)
     sign_up_page.submit_form
     sign_up_page.should be_successful
+  end
+
+  scenario 'with an invalid user' do
+    invalid_user = FactoryGirl.build(:user, password: "")
+    sign_up_page.visit_page
+    sign_up_page.fill_form(invalid_user)
+    sign_up_page.submit_form
+    sign_up_page.should_not be_successful
   end
 end
