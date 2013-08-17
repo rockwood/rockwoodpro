2bcec103cdfe42ef4123d82f3a467dcc12caa464
Kevin Rockwood <kevin@rockwood.me>
2013-08-17 16:24:27 -0400

    Add invalid sign in scenario

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,12 +1,22 @@
 require 'spec_helper'
 
 feature 'Sign in' do
-  scenario 'via the sign in page' do
-    user = FactoryGirl.create(:user)
-    sign_in_page = SignInPage.new
+  let(:sign_in_page){ SignInPage.new }
+
+  scenario 'with an existing user' do
+    existing_user = FactoryGirl.create(:user)
     sign_in_page.visit_page
-    sign_in_page.fill_form(user)
+    sign_in_page.fill_form(existing_user)
     sign_in_page.submit_form
     sign_in_page.should be_successful
   end
+
+  scenario 'with a non existing user' do
+    non_existing_user = FactoryGirl.build(:user)
+    sign_in_page.visit_page
+    sign_in_page.fill_form(non_existing_user)
+    sign_in_page.submit_form
+    sign_in_page.should_not be_successful
+  end
 end
+
