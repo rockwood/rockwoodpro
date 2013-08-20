9cd09cebb58e9e9dcf9f01dee6d6150f9ffc8791
Kevin Rockwood <kevin@rockwood.me>
2013-08-19 20:02:41 -0400

    Refactor sign_in_page api

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -5,17 +5,13 @@ feature 'Sign in' do
 
   scenario 'with an existing user' do
     existing_user = FactoryGirl.create(:user)
-    sign_in_page.visit_page
-    sign_in_page.fill_form(existing_user)
-    sign_in_page.submit_form
+    sign_in_page.sign_in(existing_user)
     sign_in_page.should be_successful
   end
 
   scenario 'with a non existing user' do
     non_existing_user = FactoryGirl.build(:user)
-    sign_in_page.visit_page
-    sign_in_page.fill_form(non_existing_user)
-    sign_in_page.submit_form
+    sign_in_page.sign_in(non_existing_user)
     sign_in_page.should_not be_successful
   end
 
