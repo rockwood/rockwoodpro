90a5dc74f409c0eb6f7659a96c6c7339c9177af0
Kevin Rockwood <kevin@rockwood.me>
2013-10-25 16:11:26 -0400

    Refactor acceptance test

--- spec/features/legacy_password_reset_spec.rb
+++ spec/features/legacy_password_reset_spec.rb
@@ -1,13 +1,11 @@
 require 'spec_helper'
 
 feature 'Resetting a legacy account' do
-  let(:sign_in_page){ SignInPage.new }
   let(:legacy_reset_page){ EditLegacyPasswordResetPage.new }
   let(:legacy_user){ FactoryGirl.create(:user, legacy: true) }
 
   scenario 'signing in with a legacy user' do
-    sign_in_page.visit_page
-    sign_in_page.sign_in(legacy_user)
+    sign_in(legacy_user)
     expect(legacy_reset_page).to be_showing
 
     legacy_reset_page.submit_form("new_password")
