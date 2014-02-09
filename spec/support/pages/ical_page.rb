07c843ea592d32866b86cbda6936659666d342d9
Kevin Rockwood <kevin@rockwood.me>
2014-02-09 17:06:20 -0500

    Display times in UTC

--- spec/support/pages/ical_page.rb
+++ spec/support/pages/ical_page.rb
@@ -9,6 +9,6 @@ class IcalPage
   end
 
   def has_event_start?(datetime)
-    has_content?("DTSTART:#{I18n.l datetime, format: :long}")
+    has_content?("DTSTART:#{I18n.l datetime, format: :ical}")
   end
 end
