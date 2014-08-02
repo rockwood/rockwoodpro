5a3691c2dccaa72753f03f3ee0719a0bf272766a
Kevin Rockwood <kevin@rockwood.me>
2014-08-02 17:33:53 -0400

    Fix admin user form

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,10 @@
 ActiveAdmin.register User do
+  controller do
+    def permitted_params
+      params.permit!
+    end
+  end
+
   member_action :switch, :method => :post do
     user = User.find(params[:id])
     sign_out
@@ -9,4 +15,18 @@ ActiveAdmin.register User do
   action_item only: :show do
     link_to('Switch to User', switch_admin_user_path(user), method: :post)
   end
+
+  form do |f|
+    f.inputs do
+      f.input :first_name
+      f.input :last_name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :admin
+      f.input :legacy
+    end
+    f.actions
+  end
+
 end
