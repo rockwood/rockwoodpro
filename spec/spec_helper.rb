c98dc0b32cac2530a7fc4898bf3362371fc27f39
Kevin Rockwood <kevin@rockwood.me>
2014-09-08 19:15:56 -0400

    Fix spec require issues
    Once and for all!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,10 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rspec'
-require 'support/pages/page'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
