3bb6fae1e4ed5055c63d944b7e9a6c25a7c61499
Kevin Rockwood <kevin@rockwood.me>
2014-11-09 13:13:46 -0500

    Fix ical event status

--- spec/features/viewing_ical_events_spec.rb
+++ spec/features/viewing_ical_events_spec.rb
@@ -1,14 +1,20 @@
 require 'spec_helper'
 
 feature 'Viewing ical events' do
-  let!(:recording) { FactoryGirl.create(:recording, datetime: Date.new(2013, 01, 01), state: :confirmed)}
+  let!(:recording) { FactoryGirl.create(:recording, datetime: Date.new(2013, 01, 01), state: :requested)}
+
   scenario do
     ical_page.visit_page
     expect(ical_page).to have_event_location(recording.location)
     expect(ical_page).to have_event_start(recording.datetime)
-    expect(ical_page).to have_event_status("CONFIRMED")
+    expect(ical_page).to have_event_status("TENTATIVE")
   end
 
+  scenario "status confirmed" do
+    recording.update(state: :confirmed)
+    ical_page.visit_page
+    expect(ical_page).to have_event_status("CONFIRMED")
+  end
 
   scenario "updating the event sequence" do
     recording.update(datetime: Date.new(2013, 01, 02))
