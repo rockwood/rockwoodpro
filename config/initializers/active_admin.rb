4e6960527811512b10d4c01eda855e431cfe7c3a
Kevin Rockwood <kevin@rockwood.me>
2015-08-11 15:29:11 -0400

    Fix admin favicon

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -7,6 +7,8 @@ ActiveAdmin.setup do |config|
   #
   config.site_title = "Rockcloud"
 
+  config.favicon = "/favicon.ico"
+
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
